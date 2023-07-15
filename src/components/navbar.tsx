3e4b90889b8ded468aaa30fba1026335dae3fb76
woaitsAryan <yoprince.ab@gmail.com>
2023-07-15 16:39:00 +0530

    first

---
+++ src/components/navbar.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
+
+export default function SwipeableTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor: string, open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent
+  ) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor: string) => (
+    <Box
+      sx={{ width: "500px" }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider variant="middle" >Socials</Divider>
+      <List>
+        {['Instagram', 'Linkedin', 'Github'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer('right', true)}><MenuIcon></MenuIcon></Button>
+      <SwipeableDrawer
+        anchor="right"
+        open={state.right}
+        onClose={toggleDrawer('right', false)}
+        onOpen={toggleDrawer('right', true)}
+      >
+        {list('right')}
+      </SwipeableDrawer>
+    </div>
+  );
+}
---
+++ src/sections/landing.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/navbar';
+
+const LandingPage = () => {
+
+  return (
+    <div>
+      {<Navbar />}
+      <h1>Welcome to the Landing Page</h1>
+      {/* Rest of your landing page content */}
+    </div>
+  );
+};
+
+export default LandingPage;
